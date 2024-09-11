38a7ee45210544cac273b811e2e9935ceab12849
Ashutosh Patil <ashutosh.patil@sportzinteractive.com>
2024-09-11 11:43:59 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/PlayersJSON.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style></style>
